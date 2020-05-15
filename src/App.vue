c0374aeb713d9924f4c01d33ae8766d9c2506dda
Magnus Lundin <magnus.lundin@gmail.com>
2020-05-15 17:16:03 +0200

    Create project - Add Vue - Add Bootstrap - Initial component

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app" class="container w-100 h-100">
+    <todo></todo>
+  </div>
+</template>
+
+<script>
+import Todo from './components/Todo';
+
+export default {
+  name: 'App',
+  components: {
+    Todo
+  }
+};
+</script>
+
+<style>
+  html, body {
+    width: 100%;
+    height: 100%;
+  }
+  body {
+    background-color: #333;
+    color: #fff;
+  }
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="row justify-content-center align-items-center h-100">
+        <div class="col-6">
+            <div class="jumbotron dark">
+                <h1>todo.</h1>
+                <p class="lead">This simple Todo-list will save your tasks locally in your browser, accessible from anywhere.</p>
+                <p>Utilizes Vue.js and Bootstrap.</p>
+                <button class="btn btn-dark btn-lg">Start using Todo</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .jumbotron.dark {
+        background-color: rgb(77, 77, 77);
+    }
+</style>goo
