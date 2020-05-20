654bd69828a13f9ca6a53d063048965c9436990b
Magnus Lundin <magnus.lundin@gmail.com>
2020-05-20 15:52:53 +0200

    Make welcome message scale good for different screen sizes

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="row justify-content-center align-items-center h-100">
-        <div class="col-6">
+        <div class="col-12 col-md-10 col-lg-8">
             <div class="jumbotron py-4 dark">
                 <h1>todo.</h1>
                 <p class="lead">This simple Todo-list will save your tasks locally in your browser, accessible from anywhere.</p>
