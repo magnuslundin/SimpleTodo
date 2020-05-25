ec152a738d79573be1b2d98e07e87d942733042d
Magnus Lundin <magnus.lundin@gmail.com>
2020-05-25 21:23:27 +0200

    Small fixes - Change to plus-icon for add button - Add item upon enter key-stroke

--- src/components/TodoAddItem.vue
+++ src/components/TodoAddItem.vue
@@ -3,18 +3,19 @@
         <div class="col-12 mb-3 py-2 rounded-bottom bg-dark">
             <div class="input-group">
                 <input 
-                    class="form-control rounded width100    "
+                    class="form-control rounded width100"
                     type="text" 
                     placeholder="What do you want to do?"
                     v-model="newItemTitle"
+                    v-on:keyup="interpretKeyStroke($event)"
                 />
                 <span class="input-group-btn">
                     <button 
                         class="btn btn-light ml-1"
-                        v-on:click="addItem ()"
+                        v-on:click="addItem()"
                         :disabled="newItemTitle.length < 3"
                     >
-                        <b-icon-play-fill></b-icon-play-fill>
+                        <b-icon-plus></b-icon-plus>
                     </button>
                 </span>
             </div>
@@ -29,6 +30,11 @@ export default {
         addItem () {
             this.$emit('itemAdded', this.newItemTitle);
             this.newItemTitle = '';
+        },
+        interpretKeyStroke (e) {
+            if (e.keyCode === 13) {
+                this.addItem ();
+            }
         }
     },
     data: function () {
